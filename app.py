136007b9a6e0ea96fa13a9cd9c304c6655e62cd1
anant1123 <115206529+anant1123@users.noreply.github.com>
2023-12-06 22:07:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pywhatkit as pwt
 pt = pd.read_pickle('pt1.pkl')
 books = pd.read_pickle('books.pkl')
 similarity_score = pd.read_pickle('similarity_score.pkl')
-popular1 = pickle.load(open('popular1.pkl', 'rb'))
-final = pickle.load(open('final_ratings.pkl', 'rb'))
+popular1 = pd.read_pickle(open('popular1.pkl', 'rb'))
+final = pd.read_pickle(open('final_ratings.pkl', 'rb'))
 
 pt1=pd.DataFrame(pt)
 final_ratings=pd.DataFrame(final)
